f919c5a6eaeb738c2abdfabf93bb814b9091c50a
tejuamirthi <tejuamirthi@gmail.com>
2020-08-27 22:50:09 +0530

    Interactor for making api calls

---
+++ ZomatoWidgetRestaurant/Interactor/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ZomatoWidgetRestaurant
+//
+//  Created by Amirthy Tejeshwar on 27/08/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+import Foundation
